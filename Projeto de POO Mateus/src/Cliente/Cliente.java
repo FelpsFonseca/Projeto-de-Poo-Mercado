bf857f3be0923436e7810c260103cf694555bae6
Mateus Mendonça <mateus.mendonca@gec.inatel.br>
2025-10-29 18:55:46 -0300

    Add files via upload

---
+++ Projeto de POO Mateus/src/Cliente/Cliente.java
@@ -0,0 +1,16 @@
+package Cliente;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public void CalculoTotalCompra(){
+        //inserir parte do codigo
+
+    }
+}
---
+++ Projeto de POO Mateus/src/Mercado/Produtos/Utensilios.java
@@ -0,0 +1,16 @@
+package Mercado.Produtos;
+
+public class Utensilios extends Produtos{
+
+    private String tipo;
+    private String marca;
+
+    public Utensilios(double preco, int quantidade, String nome){
+        super(preco, quantidade, nome);
+    }
+
+    @Override
+    public void AddProduto(){
+        //inserir parte do codigo
+    }
+}
